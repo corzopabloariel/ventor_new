138869294c0f0a572e7f98253fd44d6a0532f5b5
Pablo Corzo <corzo.pabloariel@gmail.com>
2021-01-02 00:49:36 -0300

    Command necesario, después se activa funcionalidad

---
+++ app/Console/Commands/UpdateRegister.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class UpdateRegister extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:day';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Actualización de registros TXT de la BD';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+    }
+}
