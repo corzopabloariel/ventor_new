12351a509b64d91237d236195ad8c53c27a05601
Pablo Corzo <corzo.pabloariel@gmail.com>
2021-01-14 00:26:55 -0300

    Https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -40,6 +40,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
         Paginator::useBootstrap();
         $no_img = asset("images/no-img.png");
         try {
