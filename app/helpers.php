395055fef224b99f923a312013ec511070b8fa81
Pablo Corzo <corzo.pabloariel@gmail.com>
2021-06-05 12:54:57 -0300

    Espero que ande el cron

--- app/helpers.php
+++ app/helpers.php
@@ -9,3 +9,14 @@ if (! function_exists('configs')) {
         return $default;
     }
 }
+
+if (! function_exists('fila')) {
+    function fila($loc, $lastmod, $changefreq, $priority) {
+        echo "  <url>\n";
+        echo "    <loc>".$loc."</loc>\n";
+        echo "    <lastmod>".$lastmod."</lastmod>\n";
+        echo "    <changefreq>".$changefreq."</changefreq>\n";
+        echo "    <priority>".$priority."</priority>\n";
+        echo "  </url>\n";
+    }
+}
--- routes/public.php
+++ routes/public.php
@@ -10,22 +10,11 @@ use App\Http\Controllers\Page\FormController;
 use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\ResetPasswordController;
 
-if (! function_exists('fila')) {
-    function fila($loc, $lastmod, $changefreq, $priority) {
-        echo "  <url>\n";
-        echo "    <loc>".$loc."</loc>\n";
-        echo "    <lastmod>".$lastmod."</lastmod>\n";
-        echo "    <changefreq>".$changefreq."</changefreq>\n";
-        echo "    <priority>".$priority."</priority>\n";
-        echo "  </url>\n";
-    }
-}
-
 Route::get('sitemap.php', function() {
     header ("Content-Type: text/xml");
-    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
     echo "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n";
-    $url = "https://www.ventor.com.ar/";
+    $url = "http://www.ventor.com.ar/";
     $fecha = date("Y-m-d");
     fila($url, $fecha, "monthly", "1.0");
     fila($url."empresa", $fecha, "monthly", "1.0");
@@ -41,7 +30,6 @@ Route::get('sitemap.php', function() {
         foreach($f->subparts() AS $p)
             fila($url."parte:{$f->name_slug}/subparte:{$p["slug"]}", $fecha, "monthly", "0.8");
     }
-
     echo "</urlset>\n";
 });
 Route::get('sitemap_productos.php', function() {
@@ -52,10 +40,76 @@ Route::get('sitemap_productos.php', function() {
     $fecha = date("Y-m-d");
     foreach(\App\Models\Product::all() AS $p)
         fila($url."producto:{$p->name_slug}", $fecha, "monthly", "0.8");
-
     echo "</urlset>\n";
 });
 
+// Que porquería esto no se hace
+Route::get('naaaaaaaaaaaaaaaaaaa.php', function() {
+
+    try {
+
+        // Backup collections
+        $backUpCommand = "mongoexport --uri \"mongodb://AdminVentor:56485303@127.0.0.1:27017/ventor?authsource=admin\" -c products | sed '/\"_id\":/s/\"_id\":[^,]*,//' > /home/vuserone/public_html/mongo/products.json";
+        shell_exec($backUpCommand);
+
+        $backUpCommand = "mongoexport --uri \"mongodb://AdminVentor:56485303@127.0.0.1:27017/ventor?authsource=admin\" -c orders | sed '/\"_id\":/s/\"_id\":[^,]*,//' > /home/vuserone/public_html/mongo/orders.json";
+        shell_exec($backUpCommand);
+
+        $backUpCommand = "mongoexport --uri \"mongodb://AdminVentor:56485303@127.0.0.1:27017/ventor?authsource=admin\" -c clients | sed '/\"_id\":/s/\"_id\":[^,]*,//' > /home/vuserone/public_html/mongo/clients.json";
+        shell_exec($backUpCommand);
+
+        $backUpCommand = "mongoexport --uri \"mongodb://AdminVentor:56485303@127.0.0.1:27017/ventor?authsource=admin\" -c emails | sed '/\"_id\":/s/\"_id\":[^,]*,//' > /home/vuserone/public_html/mongo/emails.json";
+        shell_exec($backUpCommand);
+
+    } catch (\Throwable $th) {
+
+        Mail::to("corzo.pabloariel@gmail.com")
+        ->send(
+            new BaseMail(
+                'Err: Backup',
+                'Backup',
+                'Ocurrió un error. Revisar "/var/www/html/public/file/log_err.txt"')
+        );
+        $log = fopen("public/file/log_err.txt", "w") or die("Unable to open file!");
+        fwrite($log, $th);
+        fclose($log);
+    }
+
+    try {
+
+        $html = "";
+        $html .= "<p>" . (new EmployeeController)->load(true) . "</p>";
+        $html .= "<p>" . (new SellerController)->load(true) . "</p>";
+        $html .= "<p>" . (new TransportController)->load(true) . "</p>";
+        $html .= "<p>" . (new ClientController)->load(true) . "</p>";
+        $html .= "<p>" . (new ProductController)->load(true) . "</p>";
+
+        Mail::to("corzo.pabloariel@gmail.com")
+        ->send(
+            new BaseMail(
+                'Update: OK',
+                'Actualizando',
+                $html)
+        );
+        $log = fopen("public/file/log_update.txt", "w") or die("Unable to open file!");
+        fwrite($log, date("Y-m-d H:i:s"));
+        fclose($log);
+
+    } catch (\Throwable $th) {
+
+        Mail::to("corzo.pabloariel@gmail.com")
+        ->send(
+            new BaseMail(
+                'Err: update',
+                'Actualizando',
+                'Ocurrió un error. Revisar "/var/www/html/public/file/log_err2.txt"')
+        );
+        $log = fopen("public/file/log_err2.txt", "w") or die("Unable to open file!");
+        fwrite($log, $th);
+        fclose($log);
+
+    }
+});
 Route::get('{link?}', [BasicController::class, 'index'])
     ->where('link' , "index|empresa|novedades|descargas|calidad|trabaje|contacto|productos")
     ->name('index');
