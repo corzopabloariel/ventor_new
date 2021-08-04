d971609c73e23d89805f503ef1ff322b3396cbd2
Pablo Corzo <corzo.pabloariel@gmail.com>
2021-08-03 22:21:00 -0300

    FIX: descargas modal help

--- resources/views/page/elements/descargas.blade.php
+++ resources/views/page/elements/descargas.blade.php
@@ -19,17 +19,25 @@
                 </div>
                 <hr>
                 <div class="mt-5">
+                    @php
+                    $download_1_type = pathinfo(config('app.static').'img/download_1.svg', PATHINFO_EXTENSION);
+                    $download_1 = 'data:image/' . $download_1_type . '+xml;base64,' . base64_encode(file_get_contents(config('app.static').'img/download_1.svg'));
+                    $download_2_type = pathinfo(config('app.static').'img/download_2.svg', PATHINFO_EXTENSION);
+                    $download_2 = 'data:image/' . $download_2_type . '+xml;base64,' . base64_encode(file_get_contents(config('app.static').'img/download_2.svg'));
+                    $download_3_type = pathinfo(config('app.static').'img/download_3.svg', PATHINFO_EXTENSION);
+                    $download_3 = 'data:image/' . $download_3_type . '+xml;base64,' . base64_encode(file_get_contents(config('app.static').'img/download_3.svg'));
+                    @endphp
                     <h3 class="text-center mb-3">Siga los siguientes pasos</h3>
                     <picture class="d-flex align-items-center">
-                        <img src="{{ asset('static/download_1.svg') }}" alt="Pasos para descarga - 1" srcset="">
+                        <img src="{{$download_1}}" alt="Pasos para descarga - 1" srcset="">
                         <p class="ml-3">Click derecho sobre el botón de descarga. Seleccionar "Guardar enlace como..."</p>
                     </picture>
                     <picture class="d-flex align-items-center mt-3">
-                        <img src="{{ asset('static/download_2.svg') }}" alt="Pasos para descarga - 2" srcset="">
+                        <img src="{{$download_2}}" alt="Pasos para descarga - 2" srcset="">
                         <p class="ml-3">Seleccione el destino de la descarga y clickee en Guardar</p>
                     </picture>
                     <picture class="d-flex align-items-center mt-3">
-                        <img src="{{ asset('static/download_3.svg') }}" alt="Pasos para descarga - 3" srcset="">
+                        <img src="{{$download_3}}" alt="Pasos para descarga - 3" srcset="">
                         <p class="ml-3">Abajo a la derecha, clickee en la flecha para desplegar el menú y luego la opción "Guardar" o "Conservar"</p>
                     </picture>
                 </div>
