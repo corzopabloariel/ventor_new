01b2e2d6db21863a39c028b6f5f2d71810155b01
Pablo Corzo <corzo.pabloariel@gmail.com>
2021-08-25 10:09:46 -0300

    Fix: carrusel de imagenes grande

--- resources/views/page/mobile/pedido.blade.php
+++ resources/views/page/mobile/pedido.blade.php
@@ -25,60 +25,6 @@
     </div>
 </div>
 @endpush
-@push('styles')
-<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
-@endpush
-@push('js')
-<script src="{{ asset('js/color.js') }}"></script>
-<script src="{{ asset('js/solver.js') }}"></script>
-<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-<script>
-const showImages = function(t) {
-    let swiper__thumbs = document.querySelector('.swiperThumbs');
-    let swiper__big = document.querySelector('.swiperBig');
-    let images = t.dataset.images.split("|");
-    let name = t.dataset.name;
-    let noimg = t.dataset.noimg;
-    document.querySelector("#imagesProductModalLabel").textContent = name;
-    /////
-    images.map((i, index) => {
-        swiper__thumbs.querySelector('.swiper-wrapper').innerHTML += `<div class="swiper-slide"><img src="${i}" onerror="this.src='${noimg}'" alt="${name}"/></div>`;
-        swiper__big.querySelector('.swiper-wrapper').innerHTML += `<div class="swiper-slide"><img src="${i}" onerror="this.src='${noimg}'" alt="${name}"/></div>`;
-    });
-    $("#imagesProductModal").modal("show");
-    $('#imagesProductModal').on('shown.bs.modal', function (e) {
-        window.swiperThumbs = new Swiper(swiper__thumbs, {
-            spaceBetween: 10,
-            slidesPerView: 4,
-            freeMode: true,
-            watchSlidesVisibility: true,
-            watchSlidesProgress: true,
-        });
-        window.swiperBig = new Swiper(swiper__big, {
-            spaceBetween: 10,
-            loop: true,
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-            },
-            thumbs: {
-                swiper: window.swiperThumbs,
-            },
-        });
-    });
-    $('#imagesProductModal').on('hidden.bs.modal', function (e) {
-        document.querySelector("#imagesProductModalLabel").textContent = '';
-        if (window.swiperThumbs) {
-            window.swiperThumbs.destroy();
-            window.swiperBig.destroy();
-            swiper__thumbs.querySelector('.swiper-wrapper').innerHTML = '';
-            swiper__big.querySelector('.swiper-wrapper').innerHTML = '';
-            swiper__big.setAttribute('style', '--swiper-navigation-color: #000; --swiper-pagination-color: #000');
-        }
-    });
-}
-</script>
-@endpush
 @includeIf('page.mobile.__filter', ['elements' => $data["lateral"]])
 <section>
     <div class="product">
