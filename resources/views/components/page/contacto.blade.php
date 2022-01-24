e848a8d301ba1f9834c98d0e8f88c83b917e49b3
Pablo Corzo - LL <corzo.pabloariel@gmail.com>
2022-01-24 19:19:06 -0300

    Avances

--- resources/views/components/page/contacto.blade.php
+++ resources/views/components/page/contacto.blade.php
@@ -1,7 +1,15 @@
 <section class="section">
     <div class="section__holder">
-        <div class="mapa">
+        <div class="section__map">
             {!! $ventor->address["mapa"] !!}
+        </div>
+    </div>
+    <div class="section__holder">
+        <h2 class="section__title">Sede Ciudad de Buenos Aires</h2>
+        <div class="section__numbers">
+            @foreach($numeros AS $number)
+                @include('components.page.contacto_number', ['element' => $number])
+            @endforeach
         </div>
     </div>
 </section>
